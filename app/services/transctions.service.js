4d3af94cdff4f6e174d1e76cba80f66a94ba5511
jecihjoy <jecihjoy@gmail.com>
2024-02-28 22:25:33 +0300

    Code cleanup

--- app/services/transctions.service.js
+++ app/services/transctions.service.js
@@ -3,54 +3,61 @@ import MpesaPayload from "../models/mpesa-payload.model";
 
 const processMpesaTransaction = async (payload) => {
   console.log("payload ", JSON.stringify(payload));
-  const transactionMetadata = payload.Body.stkCallback.CallbackMetadata.Item;
-  db.sequelize
-    .sync()
-    .then(() => {
-      MpesaPayload.create({
-        merchant_request_id: payload.Body.stkCallback.MerchantRequestID,
-        checkout_request_id: payload.Body.stkCallback.CheckoutRequestID,
-        business_short_code: "1234",
-        amount: filterData(transactionMetadata, "Amount"),
-        client_phone: filterData(transactionMetadata, "PhoneNumber"),
-        // this should be the receipt number
-        paybill_reference_number: filterData(
-          transactionMetadata,
-          "MpesaReceiptNumber"
-        ),
-        transaction_reference_number: filterData(
-          transactionMetadata,
-          "MpesaReceiptNumber"
-        ),
-        datetime: filterData(transactionMetadata, "TransactionDate"),
-        mpesa_payload: filterData(transactionMetadata, "test"),
-      })
-        .then((res) => {
-          console.log("Transaction persisted created successfully!");
+  if (payload.Body.stkCallback.CallbackMetadata.Item) {
+    const transactionMetadata = payload.Body.stkCallback.CallbackMetadata.Item;
+    db.sequelize
+      .sync()
+      .then(() => {
+        MpesaPayload.create({
+          merchant_request_id: payload.Body.stkCallback.MerchantRequestID,
+          checkout_request_id: payload.Body.stkCallback.CheckoutRequestID,
+          business_short_code: "1234",
+          amount: filterData(transactionMetadata, "Amount"),
+          client_phone: filterData(transactionMetadata, "PhoneNumber"),
+          // this should be the receipt number
+          paybill_reference_number: filterData(
+            transactionMetadata,
+            "MpesaReceiptNumber"
+          ),
+          transaction_reference_number: filterData(
+            transactionMetadata,
+            "MpesaReceiptNumber"
+          ),
+          datetime: filterData(transactionMetadata, "TransactionDate"),
+          mpesa_payload: filterData(transactionMetadata, "test"),
         })
-        .catch((error) => {
-          console.error("Failed to create a new record : ", error);
-        });
-    })
-    .catch((error) => {
-      console.error("Unable to create table : ", error);
-    });
+          .then((res) => {
+            console.log("Transaction persisted created successfully!");
+          })
+          .catch((error) => {
+            console.error("Failed to create a new record : ", error);
+          });
+      })
+      .catch((error) => {
+        console.error("Unable to create table : ", error);
+      });
+  }
 };
 
 const filterData = (data, key) => {
-  const filteredResults = data.filter((element) => element.Name === key);
+  const filteredResults = data.filter((element) => element.Name === keyItem);
   return filteredResults.length > 0 ? filteredResults[0].Value : undefined;
 };
 
 export const fetchCompletedTransactions = async (requestIDs) => {
   var requests = atob(requestIDs);
+  let queryParam = "";
+  if (requests) {
+    const programs = requests.split(",");
+    queryParam = programs.map((item) => `'${item}'`).join(",");
+  }
 
   return db.sequelize
     .authenticate()
     .then(() => {
       return db.sequelize
         .query(
-          `SELECT * FROM mpesa_transactions WHERE merchant_request_id in (${requests})`,
+          `SELECT * FROM mpesa_transactions WHERE merchant_request_id in (${queryParam})`,
           {
             type: db.sequelize.QueryTypes.SELECT,
           }
